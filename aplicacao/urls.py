7893381ca81b73254cbfe2d4a9a3269cdb0ef49c
anacstralioti <anastralioti@gmail.com>
2025-09-01 15:31:07 -0300

    atualizacoes

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="url_index"),
     path('produtos', views.produtos, name="url_produtos"),
+    path('cad_produto', views.cad_produto, name="url_cad_produto"),
+    path('atualizar_produto/<int:id>', views.atualizar_produto, name="url_atualizar_produto"),
+    path('apagar_produto/<int:id>', views.apagar_produto, name="url_apagar_produto"),
+    path('entrar', views.entrar, name="url_entrar"),
 ]
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http.response import HttpResponse
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from .models import Produto
+from django.contrib.auth.decorators import login_required
+
 from .models import Produto
 
 def index(request):
@@ -7,9 +13,65 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+@login_required
 def produtos(request):
     produtos = Produto.objects.all()
     context = {
         'produtos': produtos,
     }
     return render(request, 'produtos.html', context)
+
+def cad_produto(request):
+    if request.user.is_authenticated:
+        if request.method == "GET":
+            return render(request, 'cad_produto.html')
+        elif request.method == "POST":
+            nome = request.POST.get('nome')
+            preco = request.POST.get('preco').replace(',', '.')
+            qtde = request.POST.get('qtde')
+
+            produto = Produto(
+                nome = nome,
+                preco = preco,
+                qtde = qtde
+            )
+            produto.save()
+            return redirect('url_produtos')
+    else:
+        return redirect('url_entrar')
+
+def atualizar_produto(request, id):
+    prod = get_object_or_404(Produto, id=id)
+    if request.method == "GET":
+        context = {
+            'prod': prod,
+        }
+        return render(request, 'atualizar_produto.html', context)
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        preco = request.POST.get('preco').replace(',', '.')
+        qtde = request.POST.get('qtde')
+
+        prod.nome = nome
+        prod.preco = preco
+        prod.qtde = qtde
+        prod.save()
+    return redirect('url_produtos')
+
+def apagar_produto(request, id):
+    prod = get_object_or_404(Produto, id=id)
+    prod.delete()
+    return redirect('url_produtos')
+
+def entrar(request):
+    if request.method == "GET":
+        return render (request, "entrar.html")
+    else:
+        username = request.POST.get('nome')
+        password = request.POST.get('senha')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('url_produtos')
+        else:
+            return HttpResponse("PANE NO SISTEMA ALGUÉM ME DESCONFIGUROU")
